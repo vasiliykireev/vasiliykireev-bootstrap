9335e4d058c8151a4bd94d4095a0231ddd6f50b1
Vasiliy Kireev <vasiliy.s.kireev@gmail.com>
2024-09-22 18:42:37 +0300

    Добавил меню социальных сетей в подвал

---
+++ components/bitrix/menu/footer-social/template.php
@@ -0,0 +1,21 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
+<?if (!empty($arResult)):?>
+        <ul class="social__list ps-0 mb-0">
+        <?
+        foreach($arResult as $arItem):
+        	if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
+        		continue;
+        ?>
+                <li class="social__item social__item_link_github d-inline-block">
+                    <a
+                    class="social__link btn btn-dark focus-ring focus-ring-dark py-2 px-2"
+                    aria-current="page"
+                    href="<?=$arItem["LINK"]?>"
+                    target="_blank"
+                    title="<?=$arItem["TEXT"]?>">
+                        <?=$arItem["PARAMS"]["SVG"]?>
+                    </a>
+                </li>
+        <?endforeach?>
+    </ul>
+<?endif?>
