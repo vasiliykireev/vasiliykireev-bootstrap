063bb34471e844ee06a65b5b92b921caa52b6d9f
Vasiliy Kireev <vasiliy.s.kireev@gmail.com>
2024-09-24 17:29:37 +0300

    Вывел контент главной страницы

--- components/bitrix/news.detail/banner-main/template.php
+++ components/bitrix/news.detail/banner-main/template.php
@@ -12,6 +12,68 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
+
+<section class="first-screen">
+    <div class="container">
+        <div class="first-screen__row row-cols-1 row justify-content-center align-items-center">
+            <div class="col-12 row justify-content-center align-items-center">
+			    <?if(($arResult["DETAIL_PICTURE"] ?? '') !== ''):?>
+                    <picture class="col-12 col-md-10 col-lg-8 col-xl-8 col-xxl-7 first-screen__banner col px-0 py-3 d-block text-center">
+					    <?if(($arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP_DESKTOP_2X"] ?? '') !== ''):?>
+                            <source
+                            type="<?=$arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP_DESKTOP_2X"]["FILE_VALUE"]["CONTENT_TYPE"]?>"
+						    srcset="<?=$arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP_DESKTOP_2X"]["FILE_VALUE"]["SRC"]?>"
+                            class="first-screen__image-source first-screen__image-source_size_2x-desktop"
+                            media="(min-width: 576px) and (-webkit-min-device-pixel-ratio: 1.5)" />
+						<?endif?>
+						<?if(($arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP_DESKTOP"] ?? '') !== ''):?>
+                            <source
+                            type="<?=$arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP_DESKTOP"]["FILE_VALUE"]["CONTENT_TYPE"]?>"
+						    srcset="<?=$arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP_DESKTOP"]["FILE_VALUE"]["SRC"]?>"
+                            class="first-screen__image-source first-screen__image-source_size_desktop"
+                            media="(min-width: 576px)" />
+						<?endif?>
+						<?if(($arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP_2X"] ?? '') !== ''):?>
+                            <source
+                            type="<?=$arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP_2X"]["FILE_VALUE"]["CONTENT_TYPE"]?>"
+						    srcset="<?=$arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP_2X"]["FILE_VALUE"]["SRC"]?>"
+						    class="first-screen__image-source first-screen__image-source_size_2x-mobile"
+                            media="(max-width: 575.98px) and (-webkit-min-device-pixel-ratio: 1.5)" />
+						<?endif?>
+						<?if(($arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP"] ?? '') !== ''):?>
+                            <source
+                            type="<?=$arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP"]["FILE_VALUE"]["CONTENT_TYPE"]?>"
+						    srcset="<?=$arResult["DISPLAY_PROPERTIES"]["IMAGE_WEBP"]["FILE_VALUE"]["SRC"]?>"
+                            class="first-screen__image-source first-screen__image-source_size_mobile"
+                            media="(max-width: 575.98px)" />
+						<?endif?>
+                        <img
+                        src="<?=$arResult["DETAIL_PICTURE"]["SAFE_SRC"]?>"
+                        class="first-screen__image first-screen__image_size_default img-fluid rounded"
+                        alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>!"
+                        title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>">
+                    </picture>
+				<?endif?>
+            </div>
+            <div class="first-screen__caption col-auto pt-3 pb-5">
+				<?if(($arResult["PREVIEW_TEXT"] ?? '') !== ''):?>
+                    <h1 class="first-screen__heading h1"><?=$arResult["PREVIEW_TEXT"]?></h1>
+				<?endif?>
+				<div class="first-screen__content">
+				<?if(($arResult["DETAIL_TEXT"] ?? '') !== ''):?>
+                    <?=$arResult["DETAIL_TEXT"]?>
+				<?endif?>
+            </div>
+            <div class="first-screen__spacer col-12 py-1"></div>
+            <div class="first-screen__spacer col-12 py-1"></div>
+        </div>
+    </div>
+</section>
+<hr>
+<pre>
+	<?print_r($arResult);?>
+</pre>
+<hr>
 <div class="news-detail">
 	<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
 		<img
