803dc9d742b04826406df56cf797c0d704822bac
Vasiliy Kireev <vasiliy.s.kireev@gmail.com>
2024-09-25 18:34:29 +0300

    Вернул добавление и удаление

--- components/bitrix/news.list/thumbnails/template.php
+++ components/bitrix/news.list/thumbnails/template.php
@@ -27,6 +27,10 @@ $this->setFrameMode(true);
 		<?endif?>
         <div class="thumbnails__grid row row-cols-1 row-cols-md-2 row-cols-lg-3 gx-4 gy-5 justify-content-center align-items-center">
 		    <?foreach($arResult["ITEMS"] as $arItem):?>
+				<?
+	            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	            ?>
                 <div class="thumbnail col"id="<?=$this->GetEditAreaId($arItem['ID']);?>">
                     <div class="thumbnail__card card border-0">
 						<?if(isset($arItem["DISPLAY_PROPERTIES"]["CODE_SVG"]["VALUE"])):?>
@@ -103,10 +107,6 @@ $this->setFrameMode(true);
 	<?=$arResult["NAV_STRING"]?><br />
 <?endif;?>
 <?foreach($arResult["ITEMS"] as $arItem):?>
-	<?
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-	?>
 	<p class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
 		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
 			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
