616a0a1a8f0ecfdf210c54e52d09691b46ace089
Vasiliy Kireev <vasiliy.s.kireev@gmail.com>
2024-09-29 03:52:38 +0300

    Добавление разделов и элементов в arResult

--- components/bitrix/news/certificates/result_modifier.php
+++ components/bitrix/news/certificates/result_modifier.php
@@ -13,3 +13,64 @@ $arParams['SHARE_HANDLERS'] ??= [];
 $arParams['SHARE_HANDLERS'] = is_array($arParams['SHARE_HANDLERS']) ? $arParams['SHARE_HANDLERS'] : [];
 $arParams['SHARE_SHORTEN_URL_LOGIN'] = (string)($arParams['SHARE_SHORTEN_URL_LOGIN'] ?? 'N');
 $arParams['SHARE_SHORTEN_URL_KEY'] = (string)($arParams['SHARE_SHORTEN_URL_KEY'] ?? 'N');
+
+
+if(CModule::IncludeModule("iblock")) { 
+$arResult['XXX'] = "News List";
+/* Добавляем разделы в arResult */
+$arResult['SECTIONS'] = array();
+$arFilter = array(
+    "ACTIVE"=>"Y", // Сортировка по активности
+    'IBLOCK_ID' => $arParams['IBLOCK_ID']); 
+$arSelect = array();
+$arSections = CIBlockSection::GetList( // CIBlockSection — раздел!
+     Array("SORT"=>"ASC"),
+     $arFilter,
+     false,
+     $arSelect
+);
+while ($arSection = $arSections->GetNext()) {
+	    array_push($arResult['SECTIONS'], $arSection);
+}
+}
+
+/* Выводим элементы в arResult */
+if(isset($arResult["SECTIONS"])){
+	foreach($arResult["SECTIONS"] as &$arSection){
+		$arSection['YYY'] = "News List";
+		$arSection['ITEMS'] = array();
+$arFilter = Array(
+    'SECTION_ID' => $arSection["ID"],
+); 
+$arSelect = array();
+$arItems = CIBlockElement::GetList( // CIBlockElement — элемент!
+     Array("SORT"=>"ASC"),
+     $arFilter,
+     false,
+     $arSelect
+);
+while ($arItem = $arItems->GetNext()) {
+	array_push($arSection['ITEMS'], $arItem);
+	    // echo "<pre>";
+		// echo "arItem ";
+	    // print_r($arItem);
+		// echo "</pre>";
+}
+}
+}
+
+
+?>
+<?
+/* Добавляем элементы в разделы в arResults */
+// if(isset($arResult["SECTIONS"])){
+// 	foreach($arResult["SECTIONS"] as &$arSection){
+// 		foreach($arResult["ITEMS"] as $arItem){
+// 			if($arItem["IBLOCK_SECTION_ID"] == $arSection["ID"]){
+// 				if(!isset($arSection["ITEMS"])){
+//                     $arSection["ITEMS"] = array();}
+// 				array_push($arSection["ITEMS"], $arItem);
+// 			}
+// 		}
+// 	}
+// }
