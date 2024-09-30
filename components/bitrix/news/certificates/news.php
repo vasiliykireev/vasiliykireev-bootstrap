a1d9b49efec45ba43b47ace7695a3d6badc1905a
Vasiliy Kireev <vasiliy.s.kireev@gmail.com>
2024-09-30 19:04:27 +0300

    Добавил проброс опции AJAX Включить прокрутку к началу компонента

--- components/bitrix/news/certificates/news.php
+++ components/bitrix/news/certificates/news.php
@@ -94,6 +94,7 @@ $APPLICATION->IncludeComponent(
             	[   
             		"PARENT_SECTION" => $arSection["ID"],
             		"AJAX_MODE" => $arParams["AJAX_MODE"],
+					"AJAX_OPTION_JUMP" => $arParams["AJAX_OPTION_JUMP"],
             		"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
             		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
             		"NEWS_COUNT" => $arParams["NEWS_COUNT"],
