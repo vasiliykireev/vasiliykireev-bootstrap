5b11b9e8cc0ae51f9140f673000d0c01df1994f6
Vasiliy Kireev <vasiliy.s.kireev@gmail.com>
2024-10-02 02:02:54 +0300

    Сделал компонент хлебных крошек

--- components/bitrix/breadcrumb/breadcrumbs/template.php
+++ components/bitrix/breadcrumb/breadcrumbs/template.php
@@ -13,42 +13,49 @@ if(empty($arResult))
 
 $strReturn = '';
 
+/*
 //we can't use $APPLICATION->SetAdditionalCSS() here because we are inside the buffered function GetNavChain()
 $css = $APPLICATION->GetCSSArray();
 if(!is_array($css) || !in_array("/bitrix/css/main/font-awesome.css", $css))
 {
 	$strReturn .= '<link href="'.CUtil::GetAdditionalFileURL("/bitrix/css/main/font-awesome.css").'" type="text/css" rel="stylesheet" />'."\n";
 }
+*/
 
-$strReturn .= '<div class="bx-breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">';
+$strReturn .= '
+    <div class="breadcrumbs container">
+	<nav class="row justify-content-center" aria-label="breadcrumb">
+    <ol class="breadcrumb col-12 col-md-10 col-lg-8 col-xl-12 col-xxl-11 my-3 px-3" itemscope itemtype="https://schema.org/BreadcrumbList" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'8\' height=\'8\'%3E%3Cpath d=\'M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z\' fill=\'%236c757d\'/%3E%3C/svg%3E&#34;);">';
 
 $itemSize = count($arResult);
 for($index = 0; $index < $itemSize; $index++)
 {
 	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
-	$arrow = ($index > 0? '<i class="fa fa-angle-right"></i>' : '');
+	// $arrow = ($index > 0? '<i class="fa fa-angle-right"></i>' : '');
 
 	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
 	{
 		$strReturn .= '
-			<div class="bx-breadcrumb-item" id="bx_breadcrumb_'.$index.'" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-				'.$arrow.'
+			<li class="breadcrumb-item" id="bx_breadcrumb_'.$index.'" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+				'./*$arrow.*/'
 				<a href="'.$arResult[$index]["LINK"].'" title="'.$title.'" itemprop="item">
 					<span itemprop="name">'.$title.'</span>
 				</a>
 				<meta itemprop="position" content="'.($index + 1).'" />
-			</div>';
+			</li>';
 	}
 	else
 	{
 		$strReturn .= '
-			<div class="bx-breadcrumb-item">
-				'.$arrow.'
+			<li class="breadcrumb-item active">
+				'./*$arrow.*/'
 				<span>'.$title.'</span>
-			</div>';
+			</li>';
 	}
 }
 
-$strReturn .= '<div style="clear:both"></div></div>';
+//$strReturn .= '<div style="clear:both"></div>'.'</ol></nav>';
+$strReturn .= '</ol></nav></div>';
+
 
 return $strReturn;
