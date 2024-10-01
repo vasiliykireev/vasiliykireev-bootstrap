455a634c6a877e445829ccab9db5cbcad8df671d
Vasiliy Kireev <vasiliy.s.kireev@gmail.com>
2024-10-01 18:07:39 +0300

    Базово сделал вывод элементов

--- components/bitrix/news/articles/bitrix/news.list/.default/template.php
+++ components/bitrix/news/articles/bitrix/news.list/.default/template.php
@@ -12,6 +12,123 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
+
+<section class="portfolio pt-3 pb-5">
+        <div class="container">
+		    <?if($arParams["DISPLAY_TOP_PAGER"]):?>
+	            <?=$arResult["NAV_STRING"]?><br />
+            <?endif;?>
+
+			<?if($arResult["SECTION"]):?>
+				<div class="portfolio__header row justify-content-center pt-3 pb-2">
+                    <div class="col-auto">
+                        <h1 class="portfolio__heading"><?=$arResult["SECTION"]["PATH"][array_key_last($arResult["SECTION"]["PATH"])]["NAME"]?></h1>
+                    </div>
+                </div>
+			<?else:?>
+				<div class="portfolio__header row justify-content-center pt-3 pb-2">
+                    <div class="col-auto">
+                        <h1 class="portfolio__heading"><?=$arResult["NAME"]?></h1>
+                    </div>
+                </div>
+			<?endif?>
+
+            <div class="row justify-content-center">
+                <div class="caption col-12 col-md-10 col-lg-8 col-xl-12 col-xxl-11 mb-5">
+                    <a class="btn btn-outline-secondary" href="#">Раздел</a>
+                    <a class="btn btn-outline-primary" href="#">Портфолио</a>
+                    <a class="btn btn-outline-primary" href="#">Сайты</a>
+                </div>
+            </div>
+            <div class="example portfolio__grid row gx-4 gy-5 justify-content-center align-items-center">
+			    <?foreach($arResult["ITEMS"] as $arItem):?>
+	                <?
+	                $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	                $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	                ?>
+                    <div class="case col-12 col-md-10 col-lg-8 col-xl-12 col-xxl-11" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+                        <div class="card mb-3 border-0">
+                            <div class="row justify-content-center align-items-end">
+								<div class="article__image col-xl-5 col-xxl-5">
+									<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+		                            	<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+		                            		<a href="<?=$arItem["DETAIL_PAGE_URL"]?>">
+			    				    		<picture class="article__picture">
+                                                <!-- <source
+                                                type="image/webp"
+                                                srcset="./images/portfolio/roof-rack@2x.webp"
+                                                media="(min-width: 576px) and (-webkit-min-device-pixel-ratio: 1.5)" />
+                                                <source
+                                                type="image/webp"
+                                                srcset="./images/portfolio/roof-rack.webp"/> -->
+                                                <img
+                                                src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                                                class="img-fluid rounded"
+                                                alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                                                title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+                                                loading="lazy">
+                                            </picture>
+			    				    	</a>
+		                            	<?else:?>
+			    				    		<picture class="article__picture">
+                                                <!-- <source
+                                                type="image/webp"
+                                                srcset="./images/portfolio/roof-rack@2x.webp"
+                                                media="(min-width: 576px) and (-webkit-min-device-pixel-ratio: 1.5)" />
+                                                <source
+                                                type="image/webp"
+                                                srcset="./images/portfolio/roof-rack.webp"/> -->
+                                                <img
+                                                src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                                                class="img-fluid rounded"
+                                                alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                                                title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+                                                loading="lazy">
+                                            </picture>
+		                            	<?endif;?>
+		                            <?endif?>
+								</div>
+                                <div class="article__promo col-xl-7 col-xxl-7">
+                                    <div class="card-body pt-xl-0 pb-xl-0 px-0 px-xl-3">
+                                        <!-- <a class="card-badge d-inline-block alert alert-secondary px-2 py-1" href="#">Портфолио</a> -->
+                                        <!-- <div class="card-badges text-center text-xl-start">
+                                            <a class="card-badge btn btn-outline-secondary btn-sm mb-2" href="#">Портфолио</a>
+                                            <a class="card-badge btn btn-outline-secondary btn-sm mb-2" href="#">Сайты</a>
+                                        </div> -->
+										<?if($arItem["NAME"]):?>
+											<h2 class="h3 card-title text-center text-xl-start text-lines text-lines__amount__3">
+												<?echo $arItem["NAME"]?>
+											</h2>
+		                                <?endif;?>
+                                        <div class="news-info row mb-1">
+                                            <div class="author col small text-body-tertiary">Василий Квасов</div>
+                                            <div class="time col-auto small text-body-tertiary">11 сентября 2024</div>
+                                        </div>
+										<?if($arItem["PREVIEW_TEXT"]):?>
+                                            <p class="card-text text-body-secondary text-lines text-lines__amount__3">
+		                                    	<?echo $arItem["PREVIEW_TEXT"];?>
+										    </p>
+										<?endif;?>
+										<?if($arItem["NAME"] && !$arParams["HIDE_LINK_WHEN_NO_DETAIL"] && $arResult["USER_HAVE_ACCESS"]):?>
+                                            <p class="card-text text-center text-xl-start">
+												<a class="btn btn-primary" href="<?=$arItem["DETAIL_PAGE_URL"]?>">Подробнее</a>
+											</p>
+										<?endif?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+			    <?endforeach?>
+            </div>
+			<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+                <?=$arResult["NAV_STRING"]?>
+            <?endif;?>
+        </div>
+    </section>
+
+
+<?/*
 <div class="news-list">
 <?if($arParams["DISPLAY_TOP_PAGER"]):?>
 	<?=$arResult["NAV_STRING"]?><br />
@@ -80,7 +197,11 @@ $this->setFrameMode(true);
 		<?endforeach;?>
 	</p>
 <?endforeach;?>
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
 </div>
+*/?>
+<? $debug = true;
+if($debug){
+	echo "<pre> arResult ";
+	print_r($arResult);
+	echo "</pre>";
+}
