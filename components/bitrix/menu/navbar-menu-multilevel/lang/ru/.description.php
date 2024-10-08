c5d1876ac9a40b2ced1684ae906bad629b63f3bb
Vasiliy Kireev <vasiliy.s.kireev@gmail.com>
2024-10-08 16:31:17 +0300

    Изменение в названии компонента

--- components/bitrix/menu/navbar-menu-multilevel/lang/ru/.description.php
+++ components/bitrix/menu/navbar-menu-multilevel/lang/ru/.description.php
@@ -1,4 +1,4 @@
 <?
-$MESS ['MENU_DOT_DEFAULT_NAME'] = "Василий Киреев навигационное меню";
+$MESS ['MENU_DOT_DEFAULT_NAME'] = "Василий Киреев многоуровневое навигационное меню";
 $MESS ['MENU_DOT_DEFAULT_DESC'] = "Верхнее меню в шапке сайта";
 ?>
