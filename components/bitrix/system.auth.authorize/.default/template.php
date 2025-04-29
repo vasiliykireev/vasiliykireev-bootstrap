66556503dd690391247049e42a8a760587072f02
Vasiliy Kireev <vasiliy.s.kireev@gmail.com>
2025-04-29 22:19:24 +0300

    border-0

--- components/bitrix/system.auth.authorize/.default/template.php
+++ components/bitrix/system.auth.authorize/.default/template.php
@@ -6,7 +6,7 @@ $APPLICATION->SetTitle(GetMessage("AUTH_TITLE"));
 	<div class="auth__container container">
 		<div class="auth__info-row row justify-content-center">
 			<div class="auth__info-col col-12 col-sm-auto col-xl-4">
-				<div class="auth__card card p-3 border-1">
+				<div class="auth__card card p-3 border-0">
 					<div class="auth__info text-center">
 						<h1><?=GetMessage("AUTH_TITLE")?></h1>
 						<p><?=GetMessage("AUTH_PLEASE_AUTH")?></p>
