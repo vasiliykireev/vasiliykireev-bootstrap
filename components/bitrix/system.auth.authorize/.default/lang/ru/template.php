aeec639f2b4bc822620c95cd512a665ca2249843
Vasiliy Kireev <vasiliy.s.kireev@gmail.com>
2025-04-29 21:39:11 +0300

    Изменения в языковых файлах

--- components/bitrix/system.auth.authorize/.default/lang/ru/template.php
+++ components/bitrix/system.auth.authorize/.default/lang/ru/template.php
@@ -7,8 +7,11 @@ $MESS["AUTH_AUTHORIZE"] = "Войти";
 $MESS["AUTH_REGISTER"] = "Зарегистрироваться";
 $MESS["AUTH_FIRST_ONE"] = "Нет аккаунта?";
 $MESS["AUTH_FORGOT_PASSWORD_2"] = "Забыли пароль?";
-$MESS["AUTH_CAPTCHA_PROMT"] = "Введите буквы с картинки";
-$MESS["AUTH_TITLE"] = "Войти на сайт";
+$MESS["AUTH_CAPTCHA_PROMT"] = "Введите символы с картинки";
+$MESS["AUTH_TITLE"] = "Авторизация";
+/*
 $MESS["AUTH_SECURE_NOTE"] = "Перед отправкой формы авторизации пароль будет зашифрован в браузере. Это позволит избежать передачи пароля в открытом виде.";
 $MESS["AUTH_NONSECURE_NOTE"] = "Пароль будет отправлен в открытом виде. Включите JavaScript в браузере, чтобы зашифровать пароль перед отправкой.";
+$MESS["AUTH_RESULT_MESSAGE"] = "Неверный логин или пароль!"
+*/
 ?>
